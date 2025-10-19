7ca138079d5429bf0c327f9abb47d5a9cf61e288
Sylvia Thamu <thamumuthoni@gmail.com>
2025-10-19 23:27:16 +1000

    Add word_occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,19 +1,21 @@
 """
 Word Occurrences
 Estimate: 20 minutes
-Actual:
+Actual:   26 minutes
 """
 
 
 def main():
-    """Count occurrences of words in user-entered text (basic version)."""
+    """Count word occurrences."""
     text = input("Text: ")
     words = text.split()
     word_to_count = {}
     for word in words:
         word_to_count[word] = word_to_count.get(word, 0) + 1
-    for word, count in word_to_count.items():
-        print(f"{word} : {count}")
+
+    width = max(len(word) for word in word_to_count)
+    for word in sorted(word_to_count):
+        print(f"{word:{width}} : {word_to_count[word]}")
 
 
 main()
