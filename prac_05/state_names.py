cb353472489255a3e6747947732afd068225e981
Sylvia Thamu <thamumuthoni@gmail.com>
2025-10-19 23:24:40 +1000

    Add state_names program prac 5

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,21 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+CODE_TO_NAME = {
+    "QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory",
+    "WA": "Western Australia", "ACT": "Australian Capital Territory",
+    "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"
+}
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+state = input("Enter short state: ").upper()
+while state != "":
+    try:
+        print(state, "is", CODE_TO_NAME[state])
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state = input("Enter short state: ").upper()
+
+width = max(len(code) for code in CODE_TO_NAME)
+for code in sorted(CODE_TO_NAME):
+    print(f"{code:{width}} is {CODE_TO_NAME[code]}")
+
