1b41c8ff82a1b33bcc5ff04b6e25a670bf40f34b
Sylvia Thamu <thamumuthoni@gmail.com>
2025-10-19 14:20:37 +1000

    Add hex_colours program with constant dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical
+Hexadecimal colour lookup
+"""
+
+COLOUR_CODES = {
+    "aliceblue": "#f0f8ff",
+    "amaranth": "#e52b50",
+    "amber": "#ffbf00",
+    "beaver": "#9f8170",
+    "blue1": "#0000ff",
+    "chocolate": "#d2691e",
+    "denim": "#1560bd",
+    "dimgray": "#696969",
+    "aquamarine4": "#458b74",
+    "azure1": "#f0ffff"
+}
