1682faedb8a4a8225dbe66080c58fe9e5bce3441
Sylvia Thamu <thamumuthoni@gmail.com>
2025-11-02 15:56:38 +1000

    Add programming_language.py and languages.py prac 6

---
+++ prac_06/programming_language.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 Practical - ProgrammingLanguage class
+Estimate: 30 mins
+Actual: 50 mins
+"""
+
+
+class ProgrammingLanguage:
+    """Represent information about a programming language."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Construct a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Return string representation of a ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Return True if this language uses dynamic typing."""
+        return self.typing == "Dynamic"
